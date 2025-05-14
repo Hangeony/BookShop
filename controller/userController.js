67b26401cbc6c91b8de636be7dd9062bbb18328d
Hangeony <geonhui95@naver.com>
2025-05-14 23:24:58 +0900

    userController 구현

---
+++ controller/userController.js
@@ -0,0 +1,121 @@
+import connection from "../config/mysql.js";
+import { StatusCodes } from "http-status-codes";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import crypto from "crypto";
+
+dotenv.config();
+
+export const joinUser = (req, res) => {
+  const { email, password } = req.body;
+  console.log(`요청 바디:, ${req.body}`);
+
+  //password 암호화 해서 salt 값과 db에 저장하기
+  const salt = crypto.randomBytes(10).toString("base64");
+  const hashPwd = crypto
+    .pbkdf2Sync(password, salt, 10000, 10, "sha512")
+    .toString("base64");
+
+  let sql = "INSERT INTO users(email, password, salt) VALUES (?, ?, ?)";
+  let values = [email, hashPwd, salt];
+
+  connection.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "err발생", err });
+    }
+    console.log(`salt 값 출력 ${salt}`);
+    return res.status(StatusCodes.CREATED).json({ results });
+  });
+};
+
+export const loginUser = (req, res) => {
+  const { email, password } = req.body;
+
+  let sql = "SELECT * FROM users WHERE email =?";
+  connection.query(sql, email, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "err발생", err });
+    }
+
+    const loginUser = results[0];
+    if (loginUser && loginUser.password === password) {
+      const token = jwt.sign(
+        {
+          email: loginUser.email,
+        },
+        process.env.PRIVATE_KEY,
+        {
+          expiresIn: "5m",
+          issuer: "songa",
+        }
+      );
+
+      res.cookie("token", token, {
+        httpOnly: true,
+      });
+
+      console.log(`reqEmail확인: ${req.body.email}`);
+      console.log(`token확인: ${token}`);
+
+      return res.status(StatusCodes.OK).json(results);
+    } else {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: "비밀번호 혹은 이메일이 존재하지 않습니다." });
+    }
+  });
+};
+
+export const passwordResetRequset = (req, res) => {
+  const { email } = req.body;
+
+  let sql = "SELECT * FROM users WHERE email =?";
+  connection.query(sql, email, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "err발생", err });
+    }
+    // 이메일로 유저가 있는지 찾아봄
+    const user = results[0];
+    if (user) {
+      return res.status(StatusCodes.OK).json({
+        email: email,
+      });
+    } else {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: "요청하신 이메일이 없습니다." });
+    }
+  });
+};
+
+export const passwordRest = (req, res) => {
+  const { email, password } = req.body;
+
+  let sql = "UPDATE users SET password =? WHERE email = ?";
+  let values = [password, email];
+
+  connection.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "err발생", err });
+    }
+    if (results.affectedRows == 0) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: "이메일이 잘 못 된거같은데요 ?",
+      });
+    } else {
+      return res.status(StatusCodes.OK).json(results);
+    }
+  });
+};
--- router/users.js
+++ router/users.js
@@ -1,26 +1,24 @@
 import express from "express";
+import {
+  joinUser,
+  loginUser,
+  passwordResetRequset,
+  passwordRest,
+} from "../controller/userController.js";
 
 const router = express.Router();
 
 // 회원가입
-router.post("/join", (req, res) => {
-  res.send("회원가입");
-});
+router.post("/join", joinUser);
 
 // 로그인
-router.post("/login", (req, res) => {
-  res.send("로그인");
-});
+router.post("/login", loginUser);
 
 // 비밀번호 초기화 요청
-router.post("/reset", (req, res) => {
-  res.send("비밀번호 초기화 요청");
-});
+router.post("/reset", passwordResetRequset);
 
 // 비밀번호 초기화
-router.put("/reset", (req, res) => {
-  res.send("비밀번호 초기화");
-});
+router.put("/reset", passwordRest);
 
 const userApi = router;
 export default userApi;
